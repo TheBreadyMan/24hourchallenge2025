4bbb0fd8e044459ed67319c8f9142bef4a21db5a
dvick10 <166477118+dvick10@users.noreply.github.com>
2025-10-30 21:56:37 +1000

    Menu Button Scripts

---
+++ Assets/Scripts/UI/QuitGameButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script does not do anything by itself. Instead, a UI Button should be used to call its QuitApplication functions.
+public class QuitGameButton : MonoBehaviour
+{
+    //This function will not work inside of Editor - Application refers to the a build of the game when its launched from .exe or the like.
+    public void QuitApplication()
+    {
+        Application.Quit();
+        //Since it does not work in the Editor, a message is logged instead to show that function was called successfully.
+        Debug.Log("Exited the application");
+    }
+}
